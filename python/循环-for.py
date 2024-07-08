ea976796aff4da74e394c6c2c09f381db48254f6
puras <puras.he@gmail.com>
2024-07-08 14:28:14 +0800

    update

---
+++ python/循环-for.py
@@ -0,0 +1,16 @@
+# import turtle as t
+# t.speed(0)
+# 画三角形
+# for i in range(40):
+#     for i in range(4):
+#         t.forward(100)
+#         t.left(90)
+#     t.left(9)
+#
+# t.done()
+# 计算1 + 2 + 3 + 。。。。。+ 55
+s = 0
+for i in range(55):
+    s = s + (i + 1)
+
+print(s)
